53424696f2d2c6920ec66beb3f279c9d5770fd65
Jayden <mischievousjayden@gmail.com>
2016-08-06 12:48:31 -0400

    chapter5-javascript&jQuery

--- Amazeriffic/client/javascripts/app.js
+++ Amazeriffic/client/javascripts/app.js
@@ -1,15 +1,60 @@
-var main = function() {
+var todosjson = [
+    { 
+        "description" : "Get groceries",
+        "tags"  : [ "shopping", "chores" ]
+    },
+    { 
+        "description" : "Make up some new ToDos",
+        "tags"  : [ "writing", "work" ]
+    },
+    {
+        "description" : "Prep for Monday's class",
+        "tags"  : [ "work", "teaching" ]
+    },
+    { 
+        "description" : "Answer emails",
+        "tags"  : [ "work" ]
+    },
+    { 
+        "description" : "Take Gracie to the park",
+        "tags"  : [ "chores", "pets" ]
+    },
+    { 
+        "description" : "Finish writing this book",
+        "tags"  : [ "writing", "work" ]
+    }
+];
+
+var getToDos = function(toDoObjects) {
+    var toDos = toDoObjects.map(function(toDo) {
+        return toDo.description;
+    });
+
+    return toDos;
+}
+
+var getOrganizedByTag = function(toDoObjects) {
+    var organizedByTag = [];
+    toDoObjects.forEach(function(toDo) {
+        toDo.tags.forEach(function(tag) {
+            var flag = true;
+            organizedByTag.forEach(function(element) {
+                if(element.name === tag) {
+                    element.toDos.push(toDo.description);
+                    flag = false;
+                }
+            });
+            if(flag) {
+                organizedByTag.push({"name":tag,"toDos":[toDo.description]});
+            }
+        });
+    });
+
+    return organizedByTag;
+}
+
+var main = function(toDoObjects) {
     "use strict";
-
-    var toDos = [
-        "Get groceries",
-        "Make up some new ToDos",
-        "Prep for Monday's class",
-        "Answer emails",
-        "Take Gracie to the park",
-        "Finish writing this book"
-    ];
-
 
     $(".tabs a span").toArray().forEach(function(element) {
         var $element = $(element);
@@ -22,6 +67,8 @@ var main = function() {
             $("main .content").empty();
 
             if($element.parent().is(":nth-child(1)")) {
+                var toDos = getToDos(toDoObjects);
+
                 var i;
                 $content = $("<ul>");
                 for(i=toDos.length-1; i>=0; i--) {
@@ -29,22 +76,46 @@ var main = function() {
                 }
             }
             else if($element.parent().is(":nth-child(2)")) {
+                var toDos = getToDos(toDoObjects);
+                
                 $content = $("<ul>");
                 toDos.forEach(function(todo) {
                     $content.append($("<li>").text(todo));
                 });
             }
             else if($element.parent().is(":nth-child(3)")) {
-                var $input = $("<input>");
-                var $button = $("<button>").text("+");
+                var organizedByTag = getOrganizedByTag(toDoObjects);
 
+                $content = $("<div>");
+                organizedByTag.forEach(function(tag) {
+                    var $tagName = $("<h3>").text(tag.name);
+                    var $ul = $("<ul>");
+                    tag.toDos.forEach(function(description) {
+                        $ul.append($("<li>").text(description));
+                    });
+                    $content.append($tagName).append($ul);
+                });
+            }
+            else if($element.parent().is(":nth-child(4)")) {
+                var $input = $("<input>").addClass("description");
+                var $inputLabel = $("<p>").text("Description: ");
+                var $tagInput = $("<input>").addClass("tags");
+                var $tagLabel = $("<p>").text("Tags: ");
+                var $button = $("<button>").text("+");
                 $button.on("click", function() {
-                    if($input.val() !== "") {
-                        toDos.push($input.val());
+                    if($input.val() !== "" && $tagInput.val() !== "") {
+                        toDoObjects.push({
+                            "description":$input.val(),
+                            "tags":$tagInput.val().split(",")
+                        });
                         $input.val("");
+                        $tagInput.val("");
                     }
                 });
-                $content = $("<div>").append($input).append($button);
+
+                $content = $("<div>").append($inputLabel).append($input)
+                                    .append($tagLabel).append($tagInput)
+                                    .append($button);
             }
 
             $("main .content").append($content);
@@ -56,5 +127,7 @@ var main = function() {
     $(".tabs a:first-child span").trigger("click");
 }
 
-$(document).ready(main);
+$(document).ready(function() {
+    main(todosjson);
+});
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PracticeWebDev
 
-## 1. Amazeriffic
+## Amazeriffic
 ### Description
 This project is from textbook "Learning Web App Development" by Semmy Purewal
 
@@ -13,21 +13,4 @@ This project is from textbook "Learning Web App Development" by Semmy Purewal
 - Backend
   - Node.js
   - MongoDB
-
-## 2. Frontend
-### Descriptioin
-Practice frontend with Bootstrap, AngularJS and Anguar UI Router
-
-### Topic
-- Bootstrap
-- AngularJS
-- Angular UI Router
-
-## 3. Backend
-### Description
-Practice backend with Node.js and MongoDB
-
-### Topic
-- NodeJS
-- MongoDB
 
